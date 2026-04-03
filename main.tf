495cfe50601acea3c65a7194c1a94fd845240faf
arvindanisetti <102297339+arvindanisetti@users.noreply.github.com>
2026-04-03 16:10:39 +0530

    Update AMI ID and key name in main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-02dfbd4ff395f2a1b"
+    image_id           = "ami-01b14b7ad41e17ba4"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "march-kp"
+    key_name = "terraform"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
+     subnets = ["subnet-0364cf741213c403d", "subnet-0f4331ae5c302b407"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
